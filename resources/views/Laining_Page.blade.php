6aa24e12e1885531bbdac5e7159895effe680238
CARLOSOCHOA1 <84477731+CARLOSOCHOA1@users.noreply.github.com>
2022-06-04 18:24:02 -0600

    First commit

---
+++ resources/views/Laining_Page.blade.php
@@ -0,0 +1,198 @@
+<!doctype html>
+ <html>
+ <head>
+
+   <head>
+      <meta charset="utf-8"/>
+      <title>Laining page</title>    
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      <link rel="stylesheet" href="css/styles.css">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+   </head>
+  
+<body>
+
+   <body>
+    <header>
+     
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#top">
+                    <img src="img/log.png" alt="" width="114" height="38" class="d-inline-block align-text-top">
+                </a>
+                
+                
+    
+                <a id="navbarDropdown"  href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                   
+                 </a>
+                
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+    
+                <div class="collapse navbar-collapse" id="navmenu">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a href="#services" class="nav-link">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#HDIW" class="nav-link">Reservations</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#faq" class="nav-link">FAQ</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                About Us
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                                <li><a class="dropdown-item" href="/mission">Locations</a></li>
+                                <li><a class="dropdown-item" href="/values">Reservations</a></li>
+                                <li><a class="dropdown-item" href="/vision">Vision</a></li>
+                                <li><a class="dropdown-item" href="/values">Values</a></li>
+                              
+                               
+                            </ul>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    <!-- Showcase/Hero area -->
+    </header>
+
+   <img src="img/Hotel1.jpg" style="with: 100px; height:705px;">
+
+
+    <!-- Body -->
+    <br>
+    <br>
+    <section class="vh-40 gradient-custom" >
+        <div class="container py-5 h-5" style="position: absolute;top: 10%;left: 37%;">
+          <div class="row d-flex justify-content-center align-items-center h-50">
+            <div class="col-8 col-md-8 col-lg-6 col-xl-5">
+              <div class="card bg-dark text-white" style="border-radius: 1rem; opacity: .8;">
+                <div class="card-body p-4 text-center">
+      
+                  <div class="mb-md-1 mt-md-1 pb-1">
+                    <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                    <p class="text-white-50 mb-5">Please enter your login and password!</p>
+                    <div class="form-outline form-white mb-2">
+                      <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+                      <label class="form-label" for="typeEmailX">Email</label>
+                    </div>
+      
+                    <div class="form-outline form-white mb-2">
+                      <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+                      <label class="form-label" for="typePasswordX">Password</label>
+                    </div>
+      
+                    <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+      
+                    <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+      
+                    <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                      <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                      <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                      <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                    </div>
+                    <div>
+                        <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
+                        </p>
+                      </div>
+                  </div>
+      
+                  
+      
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    <!-- EndBody -->
+     
+     <!-- Footer -->
+     <footer class="bg-dark text-white pt-5 pb-4">
+
+        <div class="container text-center text-md-left">
+
+            <div class="row text-center text-md-left">
+
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold ">User Agreemets</h5>
+                    <p>
+                        <a href="#" class="text-white" style="text-decoration: none;"> Terms And Conditions</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white" style="text-decoration: none;"> Notice of Privacy</a>
+                    </p>
+
+
+                </div>
+
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold ">Get Help</h5>
+                    <p>
+                        <a href="#" class="text-white" style="text-decoration: none;"> FAQ</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white" style="text-decoration: none;"> How Does It Works?</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white" style="text-decoration: none;"> Sitemap</a>
+                    </p>
+
+
+                </div>
+
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold">Contact Us</h5>
+                    <p>
+                        <a href="#" class="text-white" style="text-decoration: none;"> Tepic Nayarit, Los
+                            Fresnos</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white" style="text-decoration: none;"> 311-354-4657</a>
+                    </p>
+
+                </div>
+
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold">Follow Us</h5>
+                    <p>
+                        <i class="fab fa-facebook-f "></i>
+                    </p>
+                    <p>
+                        <i class="fab fa-twitter"></i>
+                    </p>
+                    <p>
+                        <i class="fab fa-instagram"></i>
+                    </p>
+                    <p>
+                        <i class="fab fa-youtube"></i>
+                    </p>
+                </div>
+
+            </div>
+
+            <hr class="mb-5">
+
+            <div class="row align-items-center">
+                <p>©Copyright2022 | <strong class="text-warning">JobGuy</strong> | All rights reserved</p>
+            </div>
+
+        </div>
+
+    </footer>
+
+   </body>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+                
+        </script>
+
+ </html>
